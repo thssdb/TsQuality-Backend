255b3d1ebd549c7e0e14cac2165aad4faa54111f
iltyty <qiuyh0924@163.com>
2023-10-13 13:41:53 +0800

    Project init

---
+++ src/main/java/cn/edu/tsinghua/tb/Greeting.java
@@ -0,0 +1,5 @@
+package cn.edu.tsinghua.tb;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ src/test/java/cn/edu/tsinghua/tb/TbApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tsinghua.tb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
